17c2a11f5247d8bcf3ce07555dd9869901c6b4a2
vipulvij1806 <67216898+vipulvij1806@users.noreply.github.com>
2026-06-14 14:54:21 +0530

    Add social media links and editable fields support to data.js

--- data.js
+++ data.js
@@ -17,6 +17,7 @@ const PKGS = {
     qty: false,
     guestField: false,
     artistField: false,
+    editable: true,
   },
   hd: {
     label: "HD Guest Makeup",
@@ -34,6 +35,7 @@ const PKGS = {
     note: "False eyelashes & lenses included.",
     guestField: false,
     artistField: false,
+    editable: true,
   },
   basic: {
     label: "Basic Guest Makeup",
@@ -50,6 +52,7 @@ const PKGS = {
     qty: true,
     guestField: false,
     artistField: false,
+    editable: true,
   },
   special: {
     label: "Special Guest Price",
@@ -61,6 +64,7 @@ const PKGS = {
     qty: true,
     guestField: false,
     artistField: false,
+    editable: true,
   },
   salon: {
     label: "Salon Package",
@@ -72,6 +76,7 @@ const PKGS = {
     qty: false,
     guestField: true,
     artistField: false,
+    editable: true,
   },
   artist: {
     label: "Per Artist Rate",
@@ -83,6 +88,7 @@ const PKGS = {
     qty: false,
     guestField: false,
     artistField: true,
+    editable: true,
   },
 };
 
@@ -91,4 +97,6 @@ const CONTACT = {
   phone1: "+91-8107654303",
   phone2: "+91-9886008604",
   tagline: "Thank you for choosing us ✦",
+  instagram: "https://instagram.com/monika_makeovers", // Update with actual link
+  wedme: "https://wedme.in/monika-makeovers", // Update with actual link
 };
